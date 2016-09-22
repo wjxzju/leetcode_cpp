8dc6b155c7452d0cb31ae46fc94d3f04a9d9260f
willwu <wjxzju@163.com>
2016-09-22 22:59:10 +0800

    add leetcode 19

---
+++ leetcode_cpp/leetcode_19.h
@@ -0,0 +1,43 @@
+#ifndef __LEETCODE_19
+#define __LEETCODE_19
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode* p1, *p2;
+		p1 = head;
+		p2 = head;
+		for (size_t i = 0; i < n; i++)
+			p2 = p2->next;
+
+		ListNode* pre = NULL;
+		while (p2->next)
+		{
+			
+			p2 = p2->next;
+			pre = p1;
+			p1 = p1->next;
+		}
+		if (pre!=NULL){
+			pre->next = p1->next;
+			delete(p1);
+		}
+		else{
+			head = head->next;
+		}
+		return head;
+	}
+};
+
+#endif
